77fad996c1a633380e2e2180ced8f689e8e6a5c8
vitorioandy <105236604+vitorioandy@users.noreply.github.com>
2024-09-25 08:38:42 -0300

    sistema_bancario_backend

---
+++ sistema_bancário.py
@@ -0,0 +1,72 @@
+menu = """
+================ Menu ================
+
+[d] Depositar
+[s] Sacar
+[e] Ver Extrato
+[q] Sair
+
+ ========================================
+
+
+ """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saque = 0
+LIMITE_SAQUE = 3
+
+while True:
+    op= input(menu)
+        
+    if op == "d":
+    
+        valor = float(input('insira o valor a ser depositado '))
+
+        if valor > 0:
+                
+            saldo += valor
+            extrato += f"deposito: R$ {valor:.2f}\n"
+
+        else: print ('Operação falha, informe o valor corretamente.')        
+
+    elif op == "s":
+        valor = float(input('defina o valor do saque: '))
+
+        exc_saq = numero_saque >= LIMITE_SAQUE
+
+        exc_saldo = valor > saldo
+
+        exc_limite = valor > limite   
+        if exc_saq: 
+        print ("Você excedeu o seu limite de saques por hoje ")
+
+        elif exc_saldo:
+        print('Saldo insuficiente, operação cancelada')
+
+        elif exc_limite:
+        print('o limite por saque de R$500,00 foi excedido, tente um valor menor ou igual.')        
+
+        elif valor > 0:
+        saldo -= valor
+
+        extrato += f"Saque: R$ {valor:.2f}\n"
+
+        numero_saque += 1
+
+        else :
+        print ("operação falha, o valor informado é invalido")
+
+    elif op == "q":
+    print ("---------extrato----------")
+    print ("Não houveram movimentações" if not extrato else extrato)
+    print (f"\nSaldo: R$ {saldo.:2f}")
+    print ("--------------------------")
+
+    elif op == "q"
+    break
+
+else:
+    print ("operação invalida, selecione uma opção do menu")
+
---
+++ teste.py
@@ -0,0 +1,12 @@
+#estruturas condicionais 
+##maior_idade = 18
+#idade = int(input("insira sua idade"))
+
+#if idade>= 18:
+ #   print("você tem idade certa para retira sua CNH")
+#else: print("voce nao tem a idade correta ")
+
+def exibir_mensagen(nome):
+    print (f'seja bem vindo {nome}!')
+
+exibir_mensagen(nome=input(' insira seu nome: '))
